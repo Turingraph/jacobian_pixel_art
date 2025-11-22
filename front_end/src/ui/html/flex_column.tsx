9e3a013a3cc3dfd938f96d29487341c87f4b2bec
Turingraph <nemocheche@gmail.com>
2025-11-22 14:42:30 +0700

    Create flex_column.tsx

---
+++ front_end/src/ui/html/flex_column.tsx
@@ -0,0 +1,38 @@
+import { CSSProperties, JSX } from "react";
+
+import { CSS_FLEX_COLUMN, CSS_FLEX_COLUMN_CONTAINER, CSS_FLEX_COLUMN_LAST } from "./flex_column_css";
+
+export function FLEX_COLUMN({
+	id = "",
+	class_name = "" as string,
+	is_fill_app = false as boolean,
+	jsx_array
+}:{
+	id?:string,
+	class_name?:string,
+	jsx_array:JSX.Element[],
+	is_fill_app?:boolean,
+})
+{
+	let last_jsx:JSX.Element = <></>;
+	let other_jsx:JSX.Element[] = [];
+	if (jsx_array.length > 0)
+	{
+		last_jsx = jsx_array[jsx_array.length - 1];
+		if (jsx_array.length > 1)
+			other_jsx = jsx_array.slice(0, jsx_array.length - 1);
+	}
+	return <div 
+	id={id} 
+	className={class_name}
+	style={Object.assign(CSS_FLEX_COLUMN_CONTAINER, {
+		minHeight:is_fill_app?"100vh":"100%",
+		maxHeight:is_fill_app?"100vh":"100%",
+	}) as CSSProperties}
+	>
+		{other_jsx.map((item, index:number)=>{
+			return <div key={index} style={CSS_FLEX_COLUMN}>{item}</div>
+		})}
+		<div style={CSS_FLEX_COLUMN_LAST}>{last_jsx}</div>
+	</div>
+}
---
+++ front_end/src/ui/html/flex_column_css.tsx
@@ -0,0 +1,17 @@
+export const CSS_FLEX_COLUMN_CONTAINER = {
+	display:"flex",
+	flexDirection: "column",
+}
+
+export const CSS_FLEX_COLUMN = {
+	minWidth:"100%", 
+	maxWidth:"100%", 
+	backgroundColor:"lightgreen"
+}
+
+export const CSS_FLEX_COLUMN_LAST = {
+	minWidth:"100%", 
+	maxWidth:"100%", 
+	backgroundColor:"purple", 
+	flexGrow:1
+}
